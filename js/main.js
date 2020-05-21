69c1773efacec02f75d484c91ea28b8af6f05177
fk <f.jissyou@gmail.com>
2020-05-21 08:51:20 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,108 @@
+'use strict';
+
+{
+  const words = [
+    'apple',
+    'sky',
+    'blue',
+    'middle',
+    'kusune',
+    'about',
+    'bose',
+    'fish',
+    'windows',
+    'mac',
+    'letsnote',
+    'gogobar',
+    'avengers',
+    'captain',
+    'daigosuisan',
+    
+    ];
+  let word;
+  let loc;
+  let score;
+  let miss;
+  const timeLimit = 10 * 1000;
+  let startTime;
+  let isPlaying = false;
+  
+  
+  const target=document.getElementById('target');
+  const scoreLabel = document.getElementById('score');
+  const missLabel = document.getElementById('miss');
+  const timerLabel = document.getElementById('timer');
+  
+
+  function updateTarget(){
+    let placeholder = '';
+    for (let i = 0; i < loc; i++){
+      placeholder += '_';
+    }
+    target.textContent = placeholder + word.substring(loc);
+  }
+  
+  function updateTimer(){
+    const timeLeft = startTime + timeLimit - Date.now();
+    timerLabel.textContent = (timeLeft / 1000).toFixed(2);
+    
+    const timeoutId = setTimeout(() => {
+      updateTimer();
+    },10);
+    
+    
+    if (timeLeft < 0){
+      isPlaying = false;
+      
+      clearTimeout(timeoutId);
+      timerLabel.textContent = '0.00';
+      setTimeout(() => {
+        showResult();
+      },100);
+      
+      target.textContent = 'クリックしてもう一回やってみよう';
+    }
+  }
+  
+  function showResult() {
+    const accuracy = score + miss === 0 ? 0 : score / (score + miss) * 100;
+    alert(`正解文字数 ${score}, 間違い文字数 ${miss},正解率 ${accuracy.toFixed(2)}%`);
+  }
+  
+  window.addEventListener('click',() =>{
+    if (isPlaying === true){
+      return;
+    }
+    isPlaying = true;
+    
+    loc = 0;
+    score = 0;
+    miss = 0;
+    scoreLabel.textContent = score;
+    missLabel.textContent = miss;
+    word = words[Math.floor(Math.random() * words.length )];
+    
+    target.textContent = word;
+    startTime = Date.now();
+    updateTimer();
+  });
+  
+  window.addEventListener('keydown',e => {
+    if (isPlaying !== true){
+      return;
+    }
+    if (e.key === word[loc]) {
+      loc++;
+      if (loc === word.length){
+        word = words[Math.floor(Math.random() * words.length )];
+        loc = 0;
+      }
+      updateTarget();
+      score++;
+      scoreLabel.textContent = score;
+    }else{
+      miss++;
+      missLabel.textContent = miss;
+    }
+  });
+}  
